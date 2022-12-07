e05f98a17b540545d88c3e43ad92d1bb39ff9567
Aramide Mojisola <aramidemojisola7@gmail.com>
2022-12-07 22:25:29 +0100

    Print from the small number to higher number

---
+++ Javascript/assigmentCodes/logBetween.js
@@ -0,0 +1,20 @@
+/* Define a function logBetween(lowNum, highNum) that will print every number from lowNum to highNum, inclusive. 
+ *Inclusive means that the range includes lowNum and highNum.
+ * Hint: this function only needs to print using console.log it does not need to return.
+ */
+
+ // Using while loop
+ function logBetween(lowNum, highNum) {
+    let i = lowNum;
+
+    while (i < highNum) {
+        console.log(i);
+        i++;
+    }
+
+ }
+
+ logBetween(10, 20);
+
+
+git 
